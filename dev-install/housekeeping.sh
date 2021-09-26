134421b04a06bef3a72a9ce8d42e1b7ed6683879
Greg Procunier <49077358+gprocunier@users.noreply.github.com>
2021-09-26 13:09:53 -0400

    Update housekeeping.sh

--- dev-install/housekeeping.sh
+++ dev-install/housekeeping.sh
@@ -6,7 +6,7 @@
 subscription-manager release --set=8.4
 dnf -y install tmux vim ansible git vdo
 umount /home
-lvchange -an /dev/rhel_rhosp-dcib
+lvchange -an /dev/rhel_rhosp-dcib/home
 lvremove /dev/rhel_rhosp-dcib/home
 lvresize -r -l 100%FREE /dev/rhel_rhosp-dcib/root
 sed -i -e '/dcib-home/d' /etc/fstab
