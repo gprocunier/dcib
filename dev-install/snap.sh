d80589b95f5d8f4482db3f0c2b391c44ec744c1d
Greg Procunier <49077358+gprocunier@users.noreply.github.com>
2021-10-02 14:12:05 -0400

    Update snap.sh

--- dev-install/snap.sh
+++ dev-install/snap.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+[[ $(rpm -q boom-boot) ]] || (echo "install boom-boot and run with install then create to continue" && exit 1)
+
 case $1 in
   install)
     boom profile create --from-host --uname-pattern el8
-    boom create --backup --title "Root LV snapshot before changes" --rootlv rhel_rhosp-dcib/pre-deploy
+    boom create --backup --title "Root LV snapshot before changes" --rootlv rhel/pre-deploy
     grub2-mkconfig > /boot/grub2/grub.cfg
+    ;;
   create)
-    lvcreate -s rhel_rhosp-dcib/root -n pre-deploy -L 1t
+    # based on having 1tb free in the rhel vg
+    lvcreate -s rhel/root -n pre-deploy -L 1t
     ;;
   revert)
-    lvconvert --merge rhel_rhosp-dcib/pre-deploy
+    lvconvert --merge rhel/pre-deploy
     ;;
   *)
     echo  "Usage $0 : [ create | revert | install ]"
