0059bd2f4b2cc473cdd3356a1b7c18e1a3b73fe1
Greg Procunier <49077358+gprocunier@users.noreply.github.com>
2021-10-07 11:05:45 -0400

    Create hugetlb-reserve-pages.sh

---
+++ dev-install/hugetlb-reserve-pages.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+nodes_path=/sys/devices/system/node/
+if [ ! -d $nodes_path ]; then
+    echo "ERROR: $nodes_path does not exist"
+    exit 1
+fi
+
+reserve_pages()
+{
+    echo $1 > $nodes_path/$2/hugepages/hugepages-1048576kB/nr_hugepages
+}
+
+node_count=0
+for i in /sys/devices/system/node/node*
+do
+  let node_count="$node_count +1"
+done
+
+if [[ -f /etc/sysconfig/hugepages ]]
+then
+  source /etc/sysconfig/hugepages
+  # evenly distribute huge pages across all the numa domains
+  if [[ ! -z $NUM_HUGEPAGES ]]
+  then
+    hugepage_division=$[NUM_HUGEPAGES / node_count]
+    for node in ${nodes_path%\/}/node*
+    do
+      reserve_pages $hugepage_division ${node}
+    done
+  fi
+fi
